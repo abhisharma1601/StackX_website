098bccad451c5800cdccf22991518c0223a5775d
abhisharma1601 <88587455+abhisharma1601@users.noreply.github.com>
2021-10-02 00:42:12 +0530

    fd

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, flash
 import subprocess
 app = Flask(__name__)
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from firebase_admin import storage
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+# from firebase_admin import storage
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred,{
-  'projectId': 'astrodrishti2',
-  'storageBucket': 'astrodrishti2.appspot.com'
-})
+# cred = credentials.Certificate("serviceAccountKey.json")
+# firebase_admin.initialize_app(cred,{
+#   'projectId': 'astrodrishti2',
+#   'storageBucket': 'astrodrishti2.appspot.com'
+# })
 
-db = firestore.client()
+# db = firestore.client()
 
 @app.route('/')
 def index():
@@ -58,32 +58,32 @@ def host_submit():
 	subprocess.Popen([file],shell=True)
 	return render_template('thank.html')
 
-@app.route('/file_upload', methods=["POST"])
-def put_file():
-	file = request.form["kd_file"]	
-	bucket = storage.bucket()
-	blob = bucket.blob(file)
-	blob.upload_from_filename(filename=str(file))
-	blob.make_public()
-	return blob.public_url
+# @app.route('/file_upload', methods=["POST"])
+# def put_file():
+# 	file = request.form["kd_file"]	
+# 	bucket = storage.bucket()
+# 	blob = bucket.blob(file)
+# 	blob.upload_from_filename(filename=str(file))
+# 	blob.make_public()
+# 	return blob.public_url
 
-@app.route('/admin_login', methods=["POST"])
-def admin_login(): 
-	email = request.form["email"]
-	password = request.form["password"]
-	if(email=="stackx1617@gmail.com" and password=="StackX@123"):
-		Orders = []
-		key = db.collection("Orders")	
-		docs = key.stream()
-		for doc in docs:
-    			# print(f'{doc.id} => {doc.to_dict()}')
-				doc = doc.to_dict()
-				if(doc["Status"]==False):
-					Orders.append(doc)
-		print(Orders)
-		return render_template('./Admin/homepage.html', income="20000", month="January", orders = Orders)
-	else:
-		return render_template('admin.html', warning='wrong cretendials !')
+# @app.route('/admin_login', methods=["POST"])
+# def admin_login(): 
+# 	email = request.form["email"]
+# 	password = request.form["password"]
+# 	if(email=="stackx1617@gmail.com" and password=="StackX@123"):
+# 		Orders = []
+# 		key = db.collection("Orders")	
+# 		docs = key.stream()
+# 		for doc in docs:
+#     			# print(f'{doc.id} => {doc.to_dict()}')
+# 				doc = doc.to_dict()
+# 				if(doc["Status"]==False):
+# 					Orders.append(doc)
+# 		print(Orders)
+# 		return render_template('./Admin/homepage.html', income="20000", month="January", orders = Orders)
+# 	else:
+# 		return render_template('admin.html', warning='wrong cretendials !')
     	
     	
 
